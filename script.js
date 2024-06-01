e899c3106d3eb60a087e9f1a4365c9f42c6f2f84
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-06-01 09:21:26 +0530

    small issue in media query remains

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ menuBar.addEventListener('click', () => {
     })
 })
 
+
+
 var aside_lists = document.querySelectorAll('aside ul li');
 aside_lists.forEach(li => {
     li.addEventListener('click', () => {
@@ -22,6 +24,9 @@ aside_lists.forEach(li => {
     })
 })
 
+
+
+
 var chatSearchLabel = document.querySelector('.chat-search-label');
 var chatSearchInput = document.getElementById('for-chat-search');
 chatSearchInput.addEventListener('focus', () => {
@@ -31,14 +36,44 @@ chatSearchInput.addEventListener('blur', () => {
     chatSearchLabel.classList.remove('focused');
 })
 
+
+
+
 var users = document.querySelectorAll('.user-lists li');
-var chatHeader= document.querySelector('.user-chat-header')
+var chatHeader = document.querySelector('.user-chat-header')
+var chatPage = document.querySelector('.chat-page')
+var userChatPage = document.querySelector('.user-chat-page')
+var deafultChatPage = document.querySelector('.default-chat-page')
+var chatList = document.querySelector('.chat-list');
 users.forEach(user => {
     user.addEventListener('click', () => {
+        console.log(user);
         var username = user.querySelector('h4').innerText;
         var profilePic = user.querySelector('.user-list-dp').src;
-        console.log(profilePic);
         chatHeader.querySelector('.user-name').innerText = username;
         chatHeader.querySelector('.user-header-dp').src = profilePic;
+        console.log('1 is ' + chatPage.style.display);
+        console.log('2 is ' + userChatPage.style.display);
+        if (chatPage.style.display === 'none') {
+            console.log('yes its none');
+            chatPage.style.display = '';
+            chatList.style.display = 'none';
+            deafultChatPage.style.display='none'
+        }
+        else if (userChatPage.style.display == 'none') {
+            deafultChatPage.style.display = 'none';
+            userChatPage.style.display = '';
+        }
     })
+})
+
+var backChat = document.querySelector('.go-back');
+backChat.addEventListener('click', () => {
+    if (chatList.style.display === 'none') {
+        chatList.style.display = '';
+        chatPage.style.display = 'none';
+    } else {
+        userChatPage.style.display = 'none';
+        deafultChatPage.style.display = '';
+    }
 })
